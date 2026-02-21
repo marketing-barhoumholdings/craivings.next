99fa6122b08e77120311c9240c94cb426b4e7ce6
Muhammad Khatam Usmani <chkhatam124@gmail.com>
2026-02-21 10:22:48 -0800

    fix: allow studio HMR

--- next.config.js
+++ next.config.js
@@ -19,7 +19,6 @@ const nextConfig = {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       'exif-component': false,
-      'use-hot-module-reload': false,
       'rxjs/_esm5': 'rxjs',
       'rxjs/_esm5/index.js': 'rxjs',
       'sha256-uint8array': path.resolve(__dirname, 'node_modules/sha256-uint8array/lib/sha256-uint8array.js')
